b5a6fc89e6c2e5621d14986faf7015a5630ce072
jakubkuruc <kubotu332@gmail.com>
2024-07-12 21:38:15 +0200

    create or modify more sections and components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,20 @@
-const Button = ({label, iconURL}) => {
+const Button = ({label, iconURL, backgroundColor,
+  textColor, bordeColor
+}) => {
   return (
-    <button className="flex justify-center items-center
+    <button className={`flex justify-center items-center
     px-7 py-4 border font-montserrat text-lg 
-    leading-none bg-coral-red rounded-full text-white
-    border-coral-red">
+    leading-none 
+    ${backgroundColor
+    ? `${backgroundColor} ${textColor} ${bordeColor} `
+    : "bg-coral-red text-white border-coral-red" } rounded-full`}>
         {label}
 
-        <img
+        {iconURL && <img
         src={iconURL}
         alt="arrow right icon"
         className="ml-2 rounded-full w-5 h-5"
-        />
+        />}
     </button>
   )
 }
